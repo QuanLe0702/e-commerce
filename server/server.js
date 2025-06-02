32afcac51f1d3430107b0d57efadeea91fe57054
QuanLe0702 <quanle0702@gmail.com>
2025-06-02 22:49:47 +0700

    #2

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cookieParser())
 const port = process.env.PORT || 8888
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true })) 
 dbConnect()
 initRoutes(app)
 
